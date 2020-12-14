0aaa0eb0caacdba9219d131c8d065a10365e1675
MUJAKICX <mujakicdesign@gmail.com>
2020-12-14 20:51:23 +0100

    GetIndex update

--- scripts/php/getIndex.php
+++ scripts/php/getIndex.php
@@ -4,5 +4,7 @@ include_once '../../includes/dbh.inc.php';
 if(isset($_POST['getIndex'])) {
     $id = $conn->query("SELECT userid FROM studenti ORDER BY userid DESC LIMIT 1");
     $row = $id->fetch_assoc();
-    exit($row['userid']);
+    intval($row['userid']);
+    $row['userid'] += 1;
+    exit(strval($row['userid']));
 }
